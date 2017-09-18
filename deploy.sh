db12e82b72467e42418bd0d80a3327ed21d6bd75
Cheldra <Cheldra@users.noreply.github.com>
2017-09-18 15:07:54 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
+#!/bin/bash
+
 SOURCE_BRANCH="master"
 TARGET_BRANCH="files"
-
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
 
 python main.py
